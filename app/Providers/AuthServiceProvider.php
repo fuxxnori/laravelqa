a6d2ebaa9c3b354a950350d62d93b447fc74029a
fuxxnori <fuminorimamur@yahoo.co.jp>
2020-05-19 06:56:30 +0900

    Implement yupdate answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,14 +4,17 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Answer;
 use App\Question;
 use App\Policies\QuestionPolicy;
+use App\Policies\AnswerPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
 
     protected $policies = [
         Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     public function boot()
