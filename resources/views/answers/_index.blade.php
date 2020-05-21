7cd3cc5beea83d6dcc9cde185bdd530954e81519
fuxxnori <fuminorimamur@yahoo.co.jp>
2020-05-21 11:16:01 +0900

    Implement DRY principle on the application views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,47 +9,9 @@
                 @include("layouts._messages")
                 @foreach ($answers as $answer)
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                            <a title="This answer is useful"
-                                class="vote-up {{Auth::guest() ? 'off' : ''}}"
-                                onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{$answer->id}}').submit();"
-                                >
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <form id="up-vote-answer-{{$answer->id}}" action="/answers/{{$answer->id}}/vote" method="post" style="display:none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="votes-count">{{$answer->votes_count}}</span>
-                            <a title="This question is not useful"
-                                class="vote-down {{Auth::guest() ? 'off' : ''}}"
-                                onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{$answer->id}}').submit();"
-                                >
-                                <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
-                            <form id="down-vote-answer-{{$answer->id}}" action="/answers/{{$answer->id}}/vote" method="post" style="display:none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                            @can("accept",$answer)
-                                <a title="Mark this answe as best answer"
-                                    class="{{$answer->status}} mt-2"
-                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{$answer->id}}').submit();"
-                                    >
-                                    <i class="fas fa-check fa-2x"></i>
-                                </a>
-                                <form id="accept-answer-{{$answer->id}}" action="{{route('answers.accept',$answer->id)}}" method="post" style="display:none;">
-                                    @csrf
-                                </form>
-                            @else
-                                @if ($answer->is_best)
-                                <a title="The question owner accepted this answe as best answer"
-                                class="{{$answer->status}} mt-2">
-                                <i class="fas fa-check fa-2x"></i>
-                            </a> 
-                                @endif
-                            @endcan
-                        </div>
+                        @include("shared._vote",[
+                            "model"=>$answer
+                        ])
                         <div class="media-body">
                             {!! $answer->htmlbody !!}
                             <div class="row">
@@ -72,19 +34,10 @@
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                    <span class="text-muted">
-                                        Answered {{$answer->created_date}}
-                                    </span>
-                                    <div class="media mt-2">
-                                        <a href="{{$answer->user->url}}" class="pr-2">
-                                            <img src="{{$answer->user->avatar}}" width="30px" height="30px">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{$answer->user->url}}">
-                                                {{$answer->user->name}}
-                                            </a>
-                                        </div>
-                                    </div>
+                                    @include("shared._author",[
+                                        "model"=>$answer,
+                                        "label"=>"answered"
+                                    ])
                                 </div>
                             </div>
                         </div>
