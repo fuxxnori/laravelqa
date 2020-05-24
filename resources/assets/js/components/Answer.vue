ef200c8c34f5d4caa6747ced7a169218c4630f0a
fuxxnori <fuminorimamur@yahoo.co.jp>
2020-05-25 06:59:10 +0900

    Beautify the flash message and confirm dialog

--- resources/assets/js/components/Answer.vue
+++ resources/assets/js/components/Answer.vue
@@ -29,21 +29,36 @@ export default {
             .then(res => {
                 this.editing = false;
                 this.htmlbody = res.data.htmlbody;
-                alert(res.data.message);
+                this.$toast.success(res.data.message,"Success",{timeout:3000});
             })
             .catch(err => {
-                alert(err.response.data.message);
+                this.$toast.error(err.data.message,"Error",{timeout:3000});
             });
         },
         destroy(){
-            if(confirm("Are you sure??")){
+            this.$toast.question('Are you sure about that?',"Confirm",{
+            timeout: 20000,
+            close: false,
+            overlay: true,
+            displayMode: 'once',
+            id: 'question',
+            zindex: 999,
+            position: 'center',
+            buttons: [
+                ['<button><b>YES</b></button>', (instance, toast)=>{
                 axios.delete(this.endpoint)
                 .then(res=>{
                     $(this.$el).fadeOut(500,()=>{
-                        alert(res.data.message);
+                        this.$toast.success(res.data.message,"Success",{timeout:3000});;
                     })
                 });
-            }
+            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                }, true],
+                ['<button>NO</button>', function (instance, toast) {
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                }],
+            ],
+            });
         }
     },
 
