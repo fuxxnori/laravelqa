d626a3c55666134761fce4dc6db5e7b166f8e39f
fuxxnori <fuminorimamur@yahoo.co.jp>
2020-05-30 07:45:54 +0900

    Implement vue.js custom events on the answer deletion

--- public/js/app.js
+++ public/js/app.js
@@ -52028,7 +52028,7 @@ exports = module.exports = __webpack_require__(12)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -52355,6 +52355,10 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
                 (_answers = _this.answers).push.apply(_answers, _toConsumableArray(data.data));
                 _this.nextUrl = data.next_page_url;
             });
+        },
+        remove: function remove(index) {
+            this.answers.splice(index, 1);
+            this.count--;
         }
     },
 
@@ -52504,9 +52508,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 position: 'center',
                 buttons: [['<button><b>YES</b></button>', function (instance, toast) {
                     axios.delete(_this2.endpoint).then(function (res) {
-                        $(_this2.$el).fadeOut(500, function () {
-                            _this2.$toast.success(res.data.message, "Success", { timeout: 3000 });;
-                        });
+                        _this2.$emit("deleted");
                     });
                     instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
                 }, true], ['<button>NO</button>', function (instance, toast) {
@@ -52678,10 +52680,15 @@ var render = function() {
                 _vm._v(" "),
                 _c("hr"),
                 _vm._v(" "),
-                _vm._l(_vm.answers, function(answer) {
+                _vm._l(_vm.answers, function(answer, index) {
                   return _c("answer", {
                     key: answer.id,
-                    attrs: { answer: answer }
+                    attrs: { answer: answer },
+                    on: {
+                      deleted: function($event) {
+                        return _vm.remove(index)
+                      }
+                    }
                   })
                 }),
                 _vm._v(" "),
